bed29a5e3882ffc7430ea362e939cdda3159bb6c
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-07-03 21:48:00 +0200

    Create bucket internally for now

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+locals {
+  postfix = var.name_postfix != "" ? var.name_postfix : random_id.id.hex
+}
+
+resource "random_id" "id" {
+  byte_length = 8
+}
+
 data "cloudfoundry_service" "s3" {
   name = var.s3_broker_settings.service_broker
 }
@@ -7,17 +15,17 @@ data "cloudfoundry_domain" "internal" {
 }
 
 resource "cloudfoundry_app" "loki" {
-  name         = "loki"
+  name         = "tf-loki-${local.postfix}"
   space        = var.cf_space_id
   memory       = var.memory
   disk_quota   = var.disk
   docker_image = var.loki_image
   environment = merge({
     LOKI_YAML_BASE64 = base64encode(templatefile("${path.module}/templates/loki.yaml", {
-      s3_access_key        = var.s3_credentials.access_key
-      s3_secret_access_key = var.s3_credentials.secret_key
-      s3_endpoint          = var.s3_credentials.endpoint
-      s3_bucket            = var.s3_credentials.bucket
+      s3_access_key        = cloudfoundry_service_key.s3.credentials.s3_access_key
+      s3_secret_access_key = cloudfoundry_service_key.s3.credentials.s3_secret_access_key
+      s3_endpoint          = cloudfoundry_service_key.s3.credentials.s3_endpoint
+      s3_bucket            = cloudfoundry_service_key.s3.credentials.s3_bucket
     }))
   }, var.environment)
   command = "/loki/run.sh"
@@ -31,7 +39,7 @@ resource "cloudfoundry_app" "loki" {
 resource "cloudfoundry_route" "loki_internal" {
   domain   = data.cloudfoundry_domain.internal.id
   space    = var.cf_space_id
-  hostname = var.name_postfix == "" ? "loki" : "loki-${var.name_postfix}"
+  hostname = "tf-loki-${local.postfix}"
 }
 
 resource "cloudfoundry_network_policy" "loki" {
